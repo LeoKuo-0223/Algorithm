5c66d4ebf32a71799674accaca73aae4579bbdaa
LeoKuo-0223 <leo900223@gmail.com>
2022-03-02 00:03:21 +0800

    finish HW1-2

--- HW1/stack.cpp
+++ HW1/stack.cpp
@@ -1,30 +1,75 @@
-#include<string.h>
+#include<string>
 #include<iostream>
-#include<vector>
 #include <stack>
+#include<algorithm>
 #include <set>
 using namespace std;
 
+stack<set<string>> stacks;
 
 
+void PUSH(){
+    set<string> emptySet;
+    stacks.push(emptySet);
+}
+
+void DUP(){
+    set<string>topSet = stacks.top();
+    stacks.push(topSet);
+}
+
+void UNION(){
+    set<string> topSet=stacks.top();
+    stacks.pop();
+    set<string> secondSet=stacks.top();
+    stacks.pop();
+    set<string> unionSet;
+    std::set_union (topSet.begin(), topSet.end(), secondSet.begin(), secondSet.end(), std::inserter(unionSet, unionSet.begin()));
+    stacks.push(unionSet);
+}
+
+void INTERSECT(){
+    set<string> topSet=stacks.top();
+    stacks.pop();
+    set<string> secondSet=stacks.top();
+    stacks.pop();
+    set<string> intersectionSet;
+
+    std::set_intersection (topSet.begin(), topSet.end(), secondSet.begin(), secondSet.end(),std::inserter(intersectionSet, intersectionSet.begin()));
+    stacks.push(intersectionSet);
+}
+
+void ADD(){
+    set<string> topSet=stacks.top();
+    stacks.pop();
+    set<string> secondSet=stacks.top();
+    stacks.pop();
+    string newString("{");
+    for(auto it = topSet.begin(); it != topSet.end(); it++){
+        newString=newString+*it;
+    }
+    newString=newString+"}";
+    secondSet.insert(newString);
+    stacks.push(secondSet);
+}
 int main(){
     int numCommand;
-    stack<set<string>> stack;
     string command;
     cin>>numCommand;
-    for(int i=0;i<numCommand;++i){
+    for(int i=0;i<numCommand;i++){
         cin>>command;
         if(command.compare("PUSH")==0){
-
+            PUSH();
         }else if(command.compare("DUP")==0){
-
+            DUP();
         }else if(command.compare("UNION")==0){
-
+            UNION();
         }else if(command.compare("INTERSECT")==0){
-
+            INTERSECT();
         }else{
-
+            ADD();
         }
+        cout<<stacks.top().size()<<endl;
     }
     return 0;
 }
